44bb0f0f415e896b54e3f319f88d0ca3b3dd9132
Unorthodox <lawrdev@gmail.com>
2022-12-18 13:26:54 +0100

    style: navbar padding

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,45 +1,48 @@
 import Link from "next/link";
-import Head from "next/head";
+
 import Image from "next/image";
 import styles from "./Navbar.module.css";
 import { Divider } from "@mui/material";
 
 const navs = [
   { option: "Home", route: "/" },
-  { option: "Fact list", route: "/fact/2" },
   { option: "About", route: "/about" },
+  // { option: "Contact Me", route: "/" },
 ];
 
 const Navbar = () => {
   return (
-    <>
-      <Head>
-        <title>RandomFacts</title>
-        <meta name="description" content="View and share random facts" />
-        <link rel="icon" href="/owl.webp" />
-      </Head>
+    <div className={styles.navbar}>
+      <div className={styles.navbarContainer}>
+        <nav className={styles.nav}>
+          <div className={styles.logowrapper}>
+            <Image
+              src="/owl_pink.svg"
+              alt="logo"
+              width={38}
+              height={42}
+              style={{
+                objectFit: "contain",
+                objectPosition: "left center",
+                height: "auto",
+              }}
+            />
 
-      <div className={styles.navbar}>
-        <div className="content">
-          <nav className={styles.nav}>
-            <div className={styles.logowrapper}>
-              <Image src="/owl_pink.svg" alt="logo" width={32} height={56} />
-              <h1>
-                RANDOM<span className={styles.logopaint}>FACTS</span>
-              </h1>
-            </div>
+            <h1>
+              RANDOM<span className={styles.logopaint}>FACTS</span>
+            </h1>
+          </div>
 
-            {navs.map((nav, i: number) => (
-              <Link href={nav.route} className={styles.navbtn} key={i}>
-                {nav.option}
-              </Link>
-            ))}
-          </nav>
-        </div>
-
-        <Divider />
+          {navs.map((nav, i: number) => (
+            <Link href={nav.route} className={styles.navbtn} key={i}>
+              {nav.option}
+            </Link>
+          ))}
+        </nav>
       </div>
-    </>
+
+      <Divider />
+    </div>
   );
 };
 
