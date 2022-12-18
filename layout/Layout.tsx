92a8d7087fbb76fa5cf8a088ee903d51fecc4100
Unorthodox <lawrdev@gmail.com>
2022-12-18 14:05:18 +0100

    style: updates layout alignment

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,11 +1,19 @@
 import { ReactNode } from "react";
-
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import styles from "./Layout.module.css";
 interface LayoutProps {
   children: ReactNode;
 }
 
 function Layout({ children }: LayoutProps) {
-  return <>{children}</>;
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <div className="content min-h-screen">{children}</div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Layout;
